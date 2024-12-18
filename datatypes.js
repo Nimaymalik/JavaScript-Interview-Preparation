a59eb6d5784215cd1e94899f26c6f271ce2f1fc9
Nimaymalik <nimaymalik21@gmail.com>
2024-12-18 12:43:27 +0530

    datatypes
    We are learning about the datatypes widely used in Javascript for developing large-scale applications

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all Js code as newer version
+
+// alert("hello")// doesnt not work on nodejs
+
+let name = "nimay";
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+//undefined means that the vlue is not defined yet
+//Example
+//let value;
+
+//the output is
+// null=>object
+//undefined=>undefined
+
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof state]);
