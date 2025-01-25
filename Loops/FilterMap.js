ed747618ccf296f9a74d7da9f4fb842ab1e7255d
Nimaymalik <nimaymalik21@gmail.com>
2025-01-25 16:40:38 +0530

    DOM basics

--- Loops/FilterMap.js
+++ Loops/FilterMap.js
@@ -1,8 +1,8 @@
 const coding = ["js", "java", "c", "python"];
 
-// coding.forEach((key) => {
-//   console.log(key);
-// });
+coding.forEach((key) => {
+  console.log(key);
+});
 
 //filter method
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -10,10 +10,9 @@ const num = nums.filter((i) => {
   return i > 4;
 });
 
-// console.log(num)
+console.log(num);
 
 //map method
 const num1 = [1, 2, 3, 4, 5, 6, 7];
-let nu=num1.map((num) => num+10);
+let nu = num1.map((num) => num + 10);
 console.log(nu);
-
