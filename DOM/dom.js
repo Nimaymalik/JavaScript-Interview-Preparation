f71c6ff696f6fc00b4aa2db626c1cd4b01b33edd
Nimaymalik <nimaymalik21@gmail.com>
2025-01-27 13:40:33 +0530

    DOM manipuation
    completed the DOM manipulation using all the selectors

---
+++ DOM/dom.js
@@ -0,0 +1,10 @@
+document.getElementById("title");
+element.style.color = "green";
+
+element.innerHTML; // give the full text with hmtl tags also
+element.textContent; //give the content which is visible
+element.innerText; // give the full content which is not visible
+element.querySelector("h2"); //only gives the first h2 it will get
+element.querySelectorAll("h2"); //only all the h2 int the HTMl page
+
+
